cdbca7f4cb6ce6d2271e52c93545da2835f2e3ca
d-mckee <devon.l.mckee@gmail.com>
2020-09-21 00:22:29 -0700

    Adding ASCII heart to webserver start script

--- web-server/start_server.sh
+++ web-server/start_server.sh
@@ -5,6 +5,23 @@ source flask_venv/bin/activate
 export FLASK_APP=server.py
 export FLASK_ENV=development
 
+echo -e "\u001b[5m Starting Heartware web server...\u001b[0m"
+echo -e "\u001b[31m  ▄█████▄   ▄█████▄  "
+echo -e "\u001b[31m ▄█████████████████▄ "
+echo -e "\u001b[31m ███████████████████ "
+echo -e "\u001b[31m ████▓▓▓▓▓▓▓▓▓▓▓████ "
+echo -e "\u001b[31m ████▓\u001b[37mHeartware\u001b[31m▓████ "
+echo -e "\u001b[31m ████▓▓▓▓▓▓▓▓▓▓▓████ "
+echo -e "\u001b[31m █████▓\u001b[37mv0.0.1\u001b[31m▓▓█████ "
+echo -e "\u001b[31m █████▓▓▓▓▓▓▓▓▓█████ "
+echo -e "\u001b[31m  █████████████████  "
+echo -e "\u001b[31m   ███████████████   "
+echo -e "\u001b[31m    █████████████    "
+echo -e "\u001b[31m     ███████████     "
+echo -e "\u001b[31m      ▀███████▀      "
+echo -e "\u001b[0m" 
+echo -e "\u001b[4m -- Starting Heartware web server... -- \u001b[0m\n"
+
 flask run --host=0.0.0.0 -p 8080 
 
 deactivate
