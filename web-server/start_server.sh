840d8eef126737630d3806d027e1c209c76f38ea
d-mckee <devon.l.mckee@gmail.com>
2020-09-21 01:06:01 -0700

    Fixing start_server.sh to open link to web server output

--- web-server/start_server.sh
+++ web-server/start_server.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+## Enabling venv
 source flask_venv/bin/activate
 
+## Setting flask entry python file
 export FLASK_APP=server.py
 export FLASK_ENV=development
 
+## Printing ASCII art
 echo -e "\u001b[5m Starting Heartware web server...\u001b[0m"
 echo -e "\u001b[31m  ▄█████▄   ▄█████▄  "
 echo -e "\u001b[31m ▄█████████████████▄ "
@@ -20,8 +23,18 @@ echo -e "\u001b[31m    █████████████    "
 echo -e "\u001b[31m     ███████████     "
 echo -e "\u001b[31m      ▀███████▀      "
 echo -e "\u001b[0m" 
-echo -e "\u001b[4m -- Starting Heartware web server... -- \u001b[0m\n"
+echo -e "\u001b[4m -- Starting Heartware web server... -- \u001b[0m"
 
+## Printing hostname
+echo "Hostname:" $(hostname -I)
+echo
+
+## Open web server IP if -o in script (only works on WSL, oops!)
+if [[ $1 = "-o" ]]; then
+	cmd.exe start /C "http://$(hostname -I | tr -d '[:space:]'):8080"
+fi
+
+## Starting flask
 flask run --host=0.0.0.0 -p 8080 
 
 deactivate
