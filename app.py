f9aa77a987e3dbc6e897f21e0e57172849cca134
d-mckee <devon.l.mckee@gmail.com>
2020-09-25 21:42:06 -0700

    Changes to app.py, creating docs/dev-guidelines

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
+import time
+import atexit
 from flask import Flask, render_template
 from board.classes import Board
 from database.beginning import init_db, db_session
-import time
-import atexit
 from apscheduler.schedulers.background import BackgroundScheduler
 
+# Initialize database and board
 init_db()
-
 board = Board()
 
-def refreshBoard():
-    board.update(debug=False)
-
+# Initialize scheduler and register job(s)
 scheduler = BackgroundScheduler(daemon=True)
-scheduler.add_job(func=refreshBoard, trigger="interval", seconds=1)
+scheduler.add_job(func=(lambda: board.update(debug=False)), trigger="interval", seconds=1)
 scheduler.start()
-
 atexit.register(lambda: scheduler.shutdown())
 
+# Initialize Flask
 app = Flask(__name__)
 
+# Shutdown DB on app shutdown
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
 
+# Flask route bindings
 @app.route('/')
 def home():
     return render_template('home.html')
 
-if __name__ == '__main__':
-    app.run()
+# Running Flask
+app.run()
